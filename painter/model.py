8d1857eb7722ca601fc5eb3e0a902947e5d58d39
Pedro Hernandez <phernandez804@soyudemedellin.edu.co>
2024-08-06 13:25:27 -0500

    add class painter

--- painter/model.py
+++ painter/model.py
@@ -62,3 +62,32 @@ class Rectangle:
 
     def _str_(self):
         return f"Rectangle with vertices at ({self.point_1.x}, {self.point_1.y}) and ({self.point_2.x}, {self.point_2.y})"
+    
+class Painter:
+    FILE = ".painter"
+
+    def _init_(self) -> None:
+        self.shapes: list = []
+        self._load()
+
+    def _load(self) -> None:
+        try:
+            with open(Painter.FILE, "rb") as f:
+                    self.shapes = pickle.load(f)
+        except (EOFError, FileNotFoundError):
+            self.shapes = []
+
+    def _save(self) -> None:
+        with open(Painter.FILE, "wb") as f:
+            pickle.dump(self.shapes, f)
+
+    def add_shape(self, shape) -> None:
+        self.shapes.append(shape)
+        self._save()
+
+    def total_area(self) -> float:
+        return sum(shape.area() for shape in self.shapes)
+
+    def clear(self) -> None:
+        self.shapes = []
+        self._save()
