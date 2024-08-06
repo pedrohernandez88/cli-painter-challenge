cacb412a3e23a2c4a33d2ba2bb6fe37f283c1b95
Pedro Hernandez <phernandez804@soyudemedellin.edu.co>
2024-08-06 13:24:42 -0500

    add class triangle

--- painter/model.py
+++ painter/model.py
@@ -23,3 +23,25 @@ class Circle:
 
     def _str_(self):
         return f"Circle with center at ({self.center.x}, {self.center.y}) and radius {self.radius}"
+    
+class triangle:
+    def _init_(self, point_1: Point, point_2: Point, point_3: Point):
+        self.point_1 = point_1
+        self.point_2 = point_2
+        self.point_3 = point_3
+    
+    def area(self)-> float:
+        return abs((self.point_1.x * (self.point_2.y - self.point_3.y) +
+                    self.point_2.x * (self.point_3.y - self.point_1.y) +
+                    self.point_3.x * (self.point_1.y - self.point_2.y)) / 2)
+    
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x]
+        y = [self.point_1.y, self.point_2.y, self.point_3.y, self.point_1.y]
+        plt.fill(x, y, color='b')
+        plt.axis("scaled")
+        plt.show()
+    
+    def _str_(self):
+        return f"Triangle with vertices at ({self.point_1.x}, {self.point_1.y}), ({self.point_2.x}, {self.point_2.y}), and ({self.point_3.x}, {self.point_3.y})"
+    
